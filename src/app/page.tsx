017076cb414eec912ea48db3c9582752ab3a7171
SamihaKamal <samihakamal.7664@gmail.com>
2024-07-02 12:19:24 +0100

    can add todo and see todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import { prisma } from "@/db";
+import Link from "next/link";
+import { TodoItem } from "@/components/TodoItem";
 
-export default function Home() {
+
+export default async function Home() {
+
+  //Get Todo's
+  const todos = await prisma.toDo.findMany()
+
   return (
     <main>
-
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl"> Todos</h1>
+        <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+        hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map(a =>(
+          <TodoItem key={a.id} {...a}/>
+        ))}
+      </ul>
     </main>
   );
 }
